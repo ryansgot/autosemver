c7abbdf15a21e8353c12cb06b195539ce4bd54ab
Ryan Scott <ryansgot@gmail.com>
2017-07-17 16:12:18 -0700

    initial commit with building project

---
+++ autosemver/src/main/groovy/com/fsryan/gradle/autosemver/AutoSemVerPlugin.groovy
@@ -0,0 +1,11 @@
+package main.groovy.com.fsryan.gradle.autosemver
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AutoSemVerPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+
+    }
+}
